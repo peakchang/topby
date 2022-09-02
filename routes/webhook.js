13bdc11e7fa2ec5ee90fd822f249f56d5aa065e6
parkchangyong <ridebus82@nate.com>
2022-09-02 13:11:43 +0900

    asdfasdfasdf

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,20 +1,25 @@
 const express = require('express');
-
+const { Webhookdata } = require('../models');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('테스트를 해봅시다~~~~')
+router.get('/', async (req, res, next) => {
+    let wh_datas = await Webhookdata.findAll();
+    console.log(wh_datas);
+    res.render('webhookdata', {wh_datas})
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     console.log(req.body);
     let receive_json = req.body;
     let string_json = JSON.stringify(receive_json);
     console.log(string_json);
-    
+    await Webhookdata.create({webhookdata: string_json})
+
     console.log('--------------------------');
-    res.send('테스트를 해봅시다~~~~')
+    let wh_datas = await Webhookdata.findAll();
+    console.log(wh_datas);
+    res.render('webhookdata', {wh_datas})
 })
 
 
