4b715159d51dea76d18af37d5d7d58cd0b129342
parkchangyong <ridebus82@nate.com>
2022-09-03 14:56:43 +0900

    asdfasdf

--- routes/webhook.js
+++ routes/webhook.js
@@ -26,24 +26,11 @@ router.get(['/facebook', '/instagram'], function (req, res) {
 router.post('/facebook', function (req, res) {
     console.log('Facebook request body:', req.body);
 
-    if (!req.isXHubValid()) {
-        console.log('Warning - request header X-Hub-Signature not present or invalid');
-        res.sendStatus(401);
-        return;
-    }
-
-    console.log('request header X-Hub-Signature validated');
-    // Process the Facebook updates here
-    received_updates.unshift(req.body);
-    res.sendStatus(200);
 });
 
 router.post('/instagram', function (req, res) {
     console.log('Instagram request body:');
     console.log(req.body);
-    // Process the Instagram updates here
-    received_updates.unshift(req.body);
-    res.sendStatus(200);
 });
 
 
