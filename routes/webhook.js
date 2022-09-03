79d3d1737608c541985d2775094382b510bbc459
parkchangyong <ridebus82@nate.com>
2022-09-03 14:33:19 +0900

    0903 update fb

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,24 +3,81 @@ const { Webhookdata } = require('../models');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    let wh_datas = await Webhookdata.findAll();
-    console.log(wh_datas);
-    res.render('webhookdata', {wh_datas})
-})
+var token = process.env.TOKEN || 'token';
+var received_updates = [];
 
-router.post('/', async (req, res, next) => {
+
+router.get('/', function (req, res) {
+    console.log(req);
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+});
+
+router.get(['/facebook', '/instagram'], function (req, res) {
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == token
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+router.post('/facebook', function (req, res) {
+    console.log('Facebook request body:', req.body);
+
+    if (!req.isXHubValid()) {
+        console.log('Warning - request header X-Hub-Signature not present or invalid');
+        res.sendStatus(401);
+        return;
+    }
+
+    console.log('request header X-Hub-Signature validated');
+    // Process the Facebook updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
+
+router.post('/instagram', function (req, res) {
+    console.log('Instagram request body:');
     console.log(req.body);
-    let receive_json = req.body;
-    let string_json = JSON.stringify(receive_json);
-    console.log(string_json);
-    await Webhookdata.create({webhookdata: string_json})
+    // Process the Instagram updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
 
-    console.log('--------------------------');
-    let wh_datas = await Webhookdata.findAll();
-    console.log(wh_datas);
-    res.render('webhookdata', {wh_datas})
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.get('/', async (req, res, next) => {
+//     let wh_datas = await Webhookdata.findAll();
+//     console.log(wh_datas);
+//     res.render('webhookdata', {wh_datas})
+// })
+
+// router.post('/', async (req, res, next) => {
+//     console.log(req.body);
+//     let receive_json = req.body;
+//     let string_json = JSON.stringify(receive_json);
+//     console.log(string_json);
+//     await Webhookdata.create({webhookdata: string_json})
+
+//     console.log('--------------------------');
+//     let wh_datas = await Webhookdata.findAll();
+//     console.log(wh_datas);
+//     res.render('webhookdata', {wh_datas})
+// })
 
 
 module.exports = router;
