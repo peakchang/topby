6c5440891ca9b0aba541bde3e16427fd466314d3
parkchangyong <ridebus82@nate.com>
2022-09-13 15:36:49 +0900

    0913_9

--- routes/webhook.js
+++ routes/webhook.js
@@ -10,9 +10,7 @@ var received_updates = [];
 
 router.get('/', (req, res) => {
     res.send('웹훅 페이지가 준비 되었습니다.')
-});
-router.post('/', (req, res) => {
-    res.send('웹훅 페이지가 준비 되었습니다.')
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 router.post('/', (req, res) => {
     console.log('1st chk here!!!');
