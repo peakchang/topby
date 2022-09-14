4c771080631afaf155e89f36b4222e3675fe9819
parkchangyong <ridebus82@nate.com>
2022-09-15 08:34:06 +0900

    0915_2

--- routes/crm.js
+++ routes/crm.js
@@ -2,18 +2,16 @@ const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
 const User = require('../models/user');
-const Webhookdata = require('../models/webhook');
+const Wd = require('../models/webhook');
 
 
-router.get('/', isLoggedIn, (req, res, next) => {
-    wData = Webhookdata.findAll({});
-    console.log('********************************');
-    for (let i = 0; i < wData.length; i++) {
-        const element = array[i];
-        console.log(element);        
-    }
-    console.log(wData);
-    res.render('crm/crm_main');
+router.get('/', isLoggedIn, async (req, res, next) => {
+    console.log('아아니 씨발 넌 또 왜 지랄인데');
+    console.log(Wd);
+
+    wData = await Wd.findAll({});
+
+    res.render('crm/crm_main', {wData});
 })
 
 module.exports = router;
