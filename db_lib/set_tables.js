14e0b4b2b4cc999944ba315f216648a3b0a0c36d
parkchangyong <ridebus82@nate.com>
2022-10-04 11:37:23 +0900

    1004 office start

--- db_lib/set_tables.js
+++ db_lib/set_tables.js
@@ -5,6 +5,47 @@ const sql_con = require('./index');
 
 exports.tableSetting = async () => {
     console.log('테이블 셋팅 안하는거니~~~~~~~~~~~~~~');
+
+    var chkSql = 'ALTER TABLE form_status DROP COLUMN renty_status;';
+    await sql_con.promise().query(chkSql)
+
+    
+    var chkSql = 'ALTER TABLE users ADD COLUMN manage_estate VARCHAR(255) AFTER rate;';
+    await sql_con.promise().query(chkSql)
+
+    let make_memos_form = `CREATE TABLE IF NOT EXISTS memos(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        mo_phone VARCHAR(50),
+        mo_manager VARCHAR(50),
+        mo_memo VARCHAR(255),
+        created_at DATETIME DEFAULT NOW()
+    );`;
+    try {
+        sql_con.query(make_memos_form, async (err, result) => { });
+    } catch (err) {
+        console.error(err);
+    }
+
+    var chkSql = 'ALTER TABLE application_form ADD COLUMN mb_price integer AFTER mb_status;';
+    await sql_con.promise().query(chkSql)
+    
+    
+
+    
+
+    const delArray = ['itn_item', 'tv_item', 'item_other', 'mb_phone_cpn', 'mb_regnum', 'mb_email', 'mb_address', 'mb_pay_type', 'mb_bank_cpn', 'mb_bank_accountnum', 'mb_bank_name', 'mb_bank_regnum', 'mb_card_cpn', 'mb_card_cardnum', 'mb_card_name', 'mb_card_validity', 'mb_gift_bankname', 'mb_gift_accountnum', 'mb_gift_name', 'form_memo_1', 'form_memo_2', 'form_memo_3', 'form_memo_4', 'form_memo_5', 'form_memo_6', 'form_memo_7', 'form_memo_8', 'form_memo_9', 'form_memo_10'];
+
+    for (let j = 0; j < delArray.length; j++) {
+        console.log(delArray[j]);
+        let setSql = `ALTER TABLE application_form DROP COLUMN ${delArray[j]};`;
+        try {
+            await sql_con.promise().query(setSql)
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
     let make_users_form = `CREATE TABLE IF NOT EXISTS users(
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         userid VARCHAR(50) UNIQUE,
@@ -29,52 +70,9 @@ exports.tableSetting = async () => {
         form_name VARCHAR(50),
         form_type_in VARCHAR(50),
         form_location VARCHAR(50) DEFAULT '기본',
-
-
-
-        itn_item  VARCHAR(50),
-        tv_item VARCHAR(50),
-        item_other VARCHAR(255),
-
-
         mb_name VARCHAR(50) NOT NULL,
         mb_phone VARCHAR(50) NOT NULL,
-
-
-        mb_phone_cpn VARCHAR(50),
-        mb_regnum VARCHAR(50),
-        mb_email VARCHAR(50),
-        mb_address VARCHAR(150),
-        mb_pay_type VARCHAR(50),
-        mb_bank_cpn VARCHAR(50),
-        mb_bank_accountnum VARCHAR(50),
-        mb_bank_name VARCHAR(50),
-        mb_bank_regnum VARCHAR(50),
-        mb_card_cpn VARCHAR(50),
-        mb_card_cardnum VARCHAR(50),
-        mb_card_name VARCHAR(50),
-        mb_card_validity VARCHAR(50),
-        mb_gift_bankname VARCHAR(50),
-        mb_gift_accountnum VARCHAR(50),
-        mb_gift_name VARCHAR(50),
-
-
         mb_status VARCHAR(20),
-
-
-        form_memo_1 VARCHAR(255),
-        form_memo_2 VARCHAR(255),
-        form_memo_3 VARCHAR(255),
-        form_memo_4 VARCHAR(255),
-        form_memo_5 VARCHAR(255),
-        form_memo_6 VARCHAR(255),
-        form_memo_7 VARCHAR(255),
-        form_memo_8 VARCHAR(255),
-        form_memo_9 VARCHAR(255),
-        form_memo_10 VARCHAR(255),
-
-
-
         created_at DATETIME
         );`
     try {
@@ -108,7 +106,6 @@ exports.tableSetting = async () => {
 
     let makeFormTypesTable = `CREATE TABLE IF NOT EXISTS form_status(
             id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            renty_status VARCHAR(50),
             estate_status VARCHAR(50),
             estate_list VARCHAR(50)
         );`
@@ -117,68 +114,4 @@ exports.tableSetting = async () => {
     } catch (err) {
         console.error(err);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // let makeFormTypesTable = `CREATE TABLE IF NOT EXISTS form_types(
-    //     form_name VARCHAR(50),
-    //     form_type VARCHAR(50)
-    // );`
-    // try {
-    //     sql_con.query(makeFormTypesTable, async (err, result) => { });
-    // } catch (err) {
-    //     console.error(err);
-    // }
-
-
-
-
-
-
-
-
-    // ALTER TABLE reviews ADD CONRAINT rv_created_at DEFAULT ON UPDATE CURRENT_TIMESTAMP;;ST
-    // let createStatusTable = `CREATE TABLE IF NOT EXISTS set_status(
-    //     form_name VARCHAR(50),
-    //     form_type VARCHAR(50)
-    // );`
-    // try {
-    //     sql_con.query(makeFormTypesTable, async (err, result) => { });
-    // } catch (err) {
-    //     console.error(err);
-    // }
-
-    // let changeArr = ['item_other', 'form_memo_1', 'form_memo_2', 'form_memo_3', 'form_memo_4', 'form_memo_5', 'form_memo_6', 'form_memo_7', 'form_memo_8', 'form_memo_9', 'form_memo_10', 'mb_address']
-
-    // changeArr.forEach(element => {
-    //     let setSql = `ALTER TABLE application_form MODIFY COLUMN ${element} varchar(255);`
-    //     sql_con.query(setSql, (err, result) => { });
-    // });
-
-    // let chaneCNname = `ALTER TABLE application_form RENAME COLUMN createdat TO created_at;`
-    // try {
-    //     sql_con.query(chaneCNname, (err, result) => {});
-    // } catch (error) {
-
-    // }
-
 };
