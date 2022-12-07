f268e20c45a0a90a8f0fafa48fb4112d567ba4f0
parkchangyong <ridebus82@nate.com>
2022-12-07 16:15:55 +0900

    1207 endendend

--- routes/side.js
+++ routes/side.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs')
 var url = require('url');
+const { mailSender } = require('../db_lib/back_lib.js');
 
 const { setDbData, getDbData, getExLength } = require('../db_lib/back_lib.js');
 
@@ -31,6 +32,17 @@ router.use('/:name', async (req, res, next) => {
         const inserSiteDbSql = `INSERT INTO application_form (af_form_name, af_form_type_in, af_form_location, af_mb_name, af_mb_phone, af_created_at) VALUES (?,?,?,?,?,?)`;
         await sql_con.promise().query(inserSiteDbSql, [req.body.hy_set_site, '분양', 'SITE', req.body.af_mb_name, setPhone, now ]);
 
+
+        const mailSubjectManager = `${req.body.af_mb_name} 고객 DB 접수되었습니다.`;
+        const mailContentManager = `이름 : ${req.body.af_mb_name} / 전화번호 : ${setPhone}`;
+        mailSender.sendEmail(findUser.user_email, mailSubjectManager, mailContentManager);
+
+
+
+        const mailSubject = `${req.body.hy_set_site} 고객명 ${req.body.af_mb_name} 접수되었습니다.`;
+        const mailContent = `현장: ${req.body.hy_set_site} / 이름 : ${req.body.af_mb_name} / 전화번호 : ${setPhone}`;
+        mailSender.sendEmail('adpeak@naver.com', mailSubject, mailContent);
+        mailSender.sendEmail('changyong112@naver.com', mailSubject, mailContent);
 
 
         res.send(`<script type="text/javascript">alert("등록이 완료 되었습니다. 전문 상담원이 빠른 시간 내에 연락 드리도록 하겠습니다."); window.location = document.referrer; </script>`);
