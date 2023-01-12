75d4ea61f25f051bf76c487c9b107bcd265bc47e
parkchangyong <ridebus82@nate.com>
2023-01-12 12:47:11 +0900

    0112 gogogogo

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use('/lib', express.static(path.join(__dirname, 'db_lib')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 if (process.env.NODE_ENV === 'production') {
