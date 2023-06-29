7e466c82a42f229748f63dcf2d719607f0ff2559
parkchangyong <ridebus82@nate.com>
2023-06-29 16:45:51 +0900

    0629 adf;gokjsdlifjgasdf

--- routes/crm_side.js
+++ routes/crm_side.js
@@ -492,7 +492,7 @@ const chkSiteMap = async () => {
             dateStr = `${date.getFullYear()}-${date.getMonth() < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}-${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}T${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}+00:00`
         }
 
-        const tempObj = { loc: `https://adpeak.kr/${e.hy_num}`, lastmod: `${dateStr}` }
+        const tempObj = { loc: `https://adpeak.kr/side/${e.hy_num}`, lastmod: `${dateStr}` }
         return tempObj
     })
     console.log(addArr);
