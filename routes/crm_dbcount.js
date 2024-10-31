a3f2e06c8217aa6598aaf11cf25a14d96f8be5c2
park changyong <ridebus82@nate.com>
2024-10-31 11:40:05 +0900

    241031 aljsdfgogogogo

--- routes/crm_dbcount.js
+++ routes/crm_dbcount.js
@@ -7,28 +7,52 @@ const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-router.use('/', async (req, res, next) => {
+router.use('/', chkRateMaster, async (req, res, next) => {
 
     let site_list = [];
     const site_count_info_list = [];
 
+    const query = req.query
+    console.log(query);
+
+    const startSearchDate = query.sd;
+    const endSearchDate = query.ed;
+
+    let addFormQuery = ""
+    if (startSearchDate && endSearchDate) {
+        addFormQuery = ` AND af_created_at BETWEEN '${startSearchDate} 00:00:00' AND '${endSearchDate} 23:59:59'`;
+    }
+
+    const searchStr = query.search;
+    let addSearchQuery = ""
+    if (searchStr) {
+        addSearchQuery = addSearchQuery + ` WHERE sl_site_name LIKE '%${searchStr}%'`
+    }
+
+
+
+
+
     let minDate = undefined;
     let maxDate = undefined;
     try {
-        const getSiteListQuery = "SELECT * FROM site_list ORDER BY sl_id DESC LIMIT 0, 50;"
+        const getSiteListQuery = `SELECT * FROM site_list ${addSearchQuery} ORDER BY sl_id DESC LIMIT 0, 30;`
         const [rows] = await sql_con.promise().query(getSiteListQuery);
         site_list = rows;
 
         for (let i = 0; i < site_list.length; i++) {
             const siteInfo = site_list[i];
-            const getSiteDbQuery = "SELECT * FROM application_form WHERE af_form_name = ?;"
+            const getSiteDbQuery = `SELECT * FROM application_form WHERE af_form_name = ? ${addFormQuery};`
             const [dbRows] = await sql_con.promise().query(getSiteDbQuery, [siteInfo.sl_site_name]);
+
             const dates = dbRows.map(entry => entry.af_created_at).filter(date => date);
             startDate = new Date(Math.min(...dates));
             endDate = new Date(Math.max(...dates));
 
-            if (!minDate || startDate < minDate) minDate = startDate;
-            if (!maxDate || endDate > maxDate) maxDate = endDate;
+
+
+            if (!minDate || minDate == 'Invalid Date' || startDate < minDate) minDate = startDate;
+            if (!maxDate || maxDate == 'Invalid Date' || endDate > maxDate) maxDate = endDate;
 
             const result = {
                 form_name: siteInfo['sl_site_name'],
@@ -62,20 +86,14 @@ router.use('/', async (req, res, next) => {
 
     const dateArray = [];
 
+    const maxEndDate = new Date(maxDate);
+    maxEndDate.setDate(maxEndDate.getDate() + 1);
+
     // Create an array of date strings in the format 'YY-MM-DD'
-    for (let date = new Date(minDate); date <= maxDate; date.setDate(date.getDate() + 1)) {
+    for (let date = new Date(minDate); date <= maxEndDate; date.setDate(date.getDate() + 1)) {
         const dateString = date.toISOString().split('T')[0]; // Format as 'YYYY-MM-DD'
         dateArray.push(dateString.slice(2)); // Slice to get 'YY-MM-DD'
     }
-
-
-
-    console.log(site_count_info_list);
-    console.log(minDate);
-    console.log(maxDate);
-    console.log(dateArray);
-
-
 
 
     res.render('crm/work_dbcount', { site_count_info_list, dateArray });
